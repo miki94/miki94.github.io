1e8c7ad0707ed0938d411fdb5bae4b2fe9033c35
Horwat <Miki123PL@gmail.com>
2020-09-25 04:32:32 +0200

    web on znuw

---
+++ precache-manifest.5d73693b461a9e20f07b2c5bd880143a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "4306c0ac6b3255438567",
+    "url": "/js/chunk-vendors.227f8828.js"
+  },
+  {
+    "revision": "957a5a59a1cb8fa6fc10",
+    "url": "/js/app.d314e3f6.js"
+  },
+  {
+    "revision": "a7151990b77ac3707b0cbeda2ca51314",
+    "url": "/index.html"
+  },
+  {
+    "revision": "957a5a59a1cb8fa6fc10",
+    "url": "/css/app.06cbefb2.css"
+  }
+];
